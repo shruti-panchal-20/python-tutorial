b61b925c70618f21124ce6b36df978ff70fcf2de
shruti-panchal-20 <shrutikpanchal20@gmail.com>
2025-07-17 21:42:24 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#
+#print (34!=34)
+
+
+
+#print(7 < 100 and 34==34)
+
+
+#print (126>130)
+
+#print((456==456)!=(235==236))
+
+# jutuyil;open
+
+# ikujghf
+
+
+
+# lkjhgbfd
+
+
+
+# ytvnhb
+# DeprecationWarningw
+
+# returner
+# return
+# raiseer
+# raiseer
+# raiseer
+# raisee
+# r
+
+# ----17 july
+
+
+
+
+
---
+++ today.py
@@ -0,0 +1,34 @@
+# num1= int(input("Please enter your first number"))
+# num2= int(input("Please enter your second number"))
+
+
+# if num1>num2:
+#  print(f"{num1} is greater than {num2}")
+
+# elif num2>num1:
+#  print(f"{num2} is greater than {num1}")
+
+
+#else:
+# print ("Both the numbers are same ")
+
+
+
+
+
+
+# for i in range (16,1,-1):
+#  print (i)
+
+# for i in range (5,51,5):
+# #   print (i)
+
+# n = int(input("please enter your number"))
+
+# for i in range (n):
+#  print ("hello world")
+
+
+
+
+
