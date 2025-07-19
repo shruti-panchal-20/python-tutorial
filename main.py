411a79cb5bd327565d6781df983d527ac467ba89
shruti-panchal-20 <shrutikpanchal20@gmail.com>
2025-07-19 20:29:26 +0530

    Completed Data structures and added notes

--- main.py
+++ main.py
@@ -57,17 +57,117 @@
 #         rev = rev *10+ a% 10
 
 #         a=a//10
-# print(rev)
+# print(rev) 
 
 
+# def hello():
+#   print("hello this is a function by me")
 
 
+# hello()   
 
 
+# def sum(a,b):
+#   print(f"The sum of both numbers is {a+b}")
+
+# sum(3,4)
+
+# sum (5,6)
 
 
+# def hello(name,age):
+#    print(f"Your name is {name} and age is {age}")
+
+# hello("Shruti","17")
+
+#  sum(a,b=20def):
+#   print(f"The sum of numbers is {a+b}")
+
+# sum(22)  
+
+# sum(22,22)
+
+# def palindrome(st):
+#   rev = ""
+
+#   for i in range(len(st)-1,-1,-1):
+#     rev= rev + st[i]
+
+#   if rev == st:
+#     print(f"{st}  is a palindrome")
+
+#   else:
+#     print(f"{st}is not a palindrome")
+
+# palindrome("NAMAN")
+# palindrome("SHRUTI")
+
+# a = [1,2,3,4,5.6]
+
+# for i in range(len(a)):
+#     print(a[i])
 
 
+# 
 
 
+# help(list)
 
+
+# l = [2,3,4,5,6]
+
+# l.insert(2,56)
+# print (l)
+
+
+l = [3,-9,8,-6,-5,7,2]
+
+# print("positive elements are")
+# for i in l:
+#   if i >= 0:
+#     print(i)
+
+# print("negative elements are")
+# for i in l:
+#   if i < 0:
+#     print(i)
+
+# l = [34,56,78,45,99,455,99]
+
+# largest = l[0]
+# index = 0
+
+# for i in range(len(l)):
+#   if l[i] > largest:
+#     largest=l[i]
+#     index=i
+# print (f"your largest number is {largest} at index{index}")    
+
+# l = [13,12,14,15,16,17]
+
+# largest=l[0]
+# first_index=0
+# sec_largest=l[0]
+# sec_largest=0
+
+# for i in range(len(l)):
+#   if l[i] > largest:
+#     sec_largest=largest
+#     largest=l[i]
+#   elif i>sec_largest:
+#     sec_largest=i   
+
+# print(f"largest number is {largest} with index {first_index} and second largest is {sec_largest} with index{sec_largest}")    
+
+
+a=[12,13,14,15]
+
+for i in range(len(a)-1):
+  if a [i]< a[i+1]:
+    continue 
+  else:
+    print("your list is not sorted")
+    break
+
+else:
+  print("your list is sorted") 
